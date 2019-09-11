bb9efe0226f39b38388750146d57ccfbabd360ed
947418354 <wy63wy63wy63@163.com>
2019-09-11 21:52:25 +0800

    去除掉my-element-ui的坈余文件

--- src/router.js
+++ src/router.js
@@ -2,7 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import cascaderPanel from './views/cascader-panel'
 
-Vue.use(Router)
+Vue.use(Router) // Vue关联Router
+
 var router = new Router({
     routes: [
         {
